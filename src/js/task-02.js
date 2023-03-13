1c8ee4c2ef993bc492821ec0f0269871b29d9b13
SzymonSleboda <szymonskeboda@gmail.com>
2023-03-13 19:12:56 +0100

    zadanie 2 done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsElement = document.querySelector("#ingredients");
+
+console.log(ingredientsElement);
+console.log(ingredients);
+
+const listRefs = ingredients.map((ingredient) => {
+  const listRef = document.createElement("li");
+  listRef.textContent = ingredient;
+  listRef.classList.add("item");
+  return listRef;
+});
+
+ingredientsElement.append(...listRefs);
